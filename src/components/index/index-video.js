fc182c80e899473f639d7e7227a5fb6cf40c08be
Clay Coleman <jclaytoncoleman@gmail.com>
2019-02-26 18:24:19 -0700

    aligned videos to center and reformatted file

--- src/components/index/index-video.js
+++ src/components/index/index-video.js
@@ -13,11 +13,20 @@ export default class IndexVideo extends Component {
     };
 
     return (
-      <div>
-        <h4 css={{
-          textAlign: "center",
-        }}>{this.props.videoData.title}</h4>
-        <YouTube css={{ maxWidth: "100%" }} videoId={this.props.videoData.id} opts={opts} onReady={this._onReady} />
+      <div css={{ textAlign: 'center' }}>
+        <h4
+          css={{
+            textAlign: 'center',
+          }}
+        >
+          {this.props.videoData.title}
+        </h4>
+        <YouTube
+          css={{ maxWidth: '100%' }}
+          videoId={this.props.videoData.id}
+          opts={opts}
+          onReady={this._onReady}
+        />
       </div>
     );
   }
