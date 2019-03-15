69bea0b12f9fe2907ee2b6be833f771299ae3a9a
Clay Coleman <jclaytoncoleman@gmail.com>
2019-03-15 08:00:08 -0600

    added portal data

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -16,6 +16,109 @@ export const STUDENT_VIDEOS = [
   },
 ];
 
+export const PORTAL_TABS = [
+  {
+    title: 'English',
+    extraLinks: [
+      {
+        title: 'Grammar Explanations and Practice',
+        link: 'https://www.khanacademy.org/humanities/grammar#parts-of-speech-the-noun',
+      },
+    ],
+    mainYoutubeLink: 'eTD_Jo5zsIA',
+    extraCreditText: 'Complete Extra Practice on pg. 422',
+  },
+  {
+    title: 'Math',
+    extraLinks: [
+      {
+        title: 'Long Division/Remainders',
+        link: 'https://www.khanacademy.org/math/arithmetic/arith-review-multiply-divide/modal/v/long-division-with-remainder-example',
+      },
+      {
+        title: 'Pythagorean Theorem',
+        link: 'https://www.khanacademy.org/math/geometry/hs-geo-trig/modal/v/pythagorean-theorem-2',
+      },
+      {
+        title: 'Distance',
+        link: 'https://www.khanacademy.org/math/basic-geo/basic-geometry-pythagorean-theorem/pythagorean-theorem-distance/v/distance-formula',
+      },
+      {
+        title: 'Midpoints',
+        link: 'https://www.khanacademy.org/math/geometry/hs-geo-analytic-geometry/hs-geo-distance-and-midpoints/v/midpoint-formula',
+      },
+      {
+        title: 'Imaginary Numbers',
+        link: 'https://www.khanacademy.org/math/precalculus/imaginary-and-complex-numbers/modal/v/introduction-to-i-and-imaginary-numbers',
+      },
+      {
+        title: 'Matrices',
+        link: 'https://www.khanacademy.org/math/precalculus/precalc-matrices/intro-to-matrices/v/introduction-to-the-matrix?modal=1',
+      },
+      {
+        title: 'Vectors',
+        link: 'https://www.khanacademy.org/math/precalculus/vectors-precalc/modal/v/introduction-to-vectors-and-scalars',
+      },
+      {
+        title: 'Adding Functions',
+        link: 'https://www.khanacademy.org/math/algebra2/manipulating-functions/combining-functions/v/sum-of-functions',
+      },
+      {
+        title: 'Dividing Functions',
+        link: 'https://www.khanacademy.org/math/algebra2/manipulating-functions/combining-functions/v/quotient-of-functions',
+      },
+      {
+        title: 'Graphing Quadratics/Parabolas',
+        link: 'https://www.khanacademy.org/math/algebra/quadratics/parabolas-intro-alg1/v/parabolas-intro',
+      },
+      {
+        title: 'Compound Probability',
+        link: 'https://www.khanacademy.org/math/ap-statistics/probability-ap/probability-multiplication-rule/v/compound-probability-of-independent-events',
+      },
+      {
+        title: 'Standard Deviation',
+        link: 'https://www.khanacademy.org/math/ap-statistics/summarizing-quantitative-data-ap/measuring-spread-quantitative/v/sample-standard-deviation-and-bias',
+      },
+    ],
+    mainYoutubeLink: 'uMx-0o8Dtzg',
+    extraCreditText: 'Finish reading section on pg. 455',
+  },
+  {
+    title: 'Reading',
+    extraLinks: [
+      {
+        title: 'Active Reading',
+        link: 'https://www.youtube.com/watch?v=wKuMY85G3zQ',
+      },
+    ],
+    mainYoutubeLink: '8-4wxcnRFuQ',
+    extraCreditText: 'Complete Reading Tests #2 and #3',
+  },
+  {
+    title: 'Science',
+    extraLinks: [
+      {
+        title: 'Reading',
+        link: 'https://www.youtube.com/watch?v=rg4XNLxD6uM',
+      },
+      {
+        title: 'General Tips',
+        link: 'https://www.youtube.com/watch?v=Xu9ziq4yhOk',
+      },
+      {
+        title: 'Figures Questions',
+        link: 'https://www.youtube.com/watch?v=lh6eU18n6OI',
+      },
+      {
+        title: 'Pattern Questions',
+        link: 'https://www.youtube.com/watch?v=O64f4FOelFA',
+      },
+    ],
+    mainYoutubeLink: 'HyrBPX_OKbQ',
+    extraCreditText: 'Finish reading section on pg. 624',
+  },
+];
+
 export const RESPONSIVE_OUTER_MARGINS = {
   marginLeft: '1.75rem',
   marginRight: '1.75rem',
